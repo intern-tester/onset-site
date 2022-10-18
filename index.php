73c13860f6f8c730784ca599a7282569b0fbb13f
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-18 15:27:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ function bts($title = "", $content = "", $lang = "en"){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>'..'</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   </head>
   <body>
     '.$content.'
