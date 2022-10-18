6da98b2ea768c0b20d2d99ade922b37e2e233e92
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-18 17:42:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ print $site->bts("Home", '
     </div>
   </div>
   
-  <footer class="py-2 my-4">
+  <footer class="py-1 my-2">
     <p class="text-center text-muted">© '.date('Y').'  '.getenv('logo').'  Company, Inc</p>
   </footer>
   
