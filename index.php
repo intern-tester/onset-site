ec51f1bf51d454cf9f528b69d21dcd4f0af4a11b
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-18 17:30:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ print $site->bts("Home", '
 <div class="px-4 pt-5 my-5 text-center border-bottom">
     <h1 class="display-4">'.getenv('logo').'</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">ONSET it\'s small team. We work on simple codes. We work only for itself. All our projects do not use like professional usage. If you use our code and find some troubles, please contact us by Github. </p>
+      <p class="lead mb-4">'.getenv('description').'</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
         <a href="'.getenv('git-href').'"><i class="bi bi-github fs-1"></i></a>
       </div>
