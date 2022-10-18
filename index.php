1c5782aed20e7da5c372162ce49ab1d5aec9145d
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-18 16:02:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ print $site->bts("Home", '
 <div class="px-4 pt-5 my-5 text-center border-bottom">
     <h1 class="display-4">ONSET</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4"></p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Projects</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4" data-bs-toggle="modal" data-bs-target="#modal-about" >About Us</button>
+      <p class="lead mb-4">ONSET it\'s small team. We work on simple codes. We work only for itself. All our projects do not use like professional usage. If you use our code and find some troubles, please contact us by Telegram or Github. </p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5" hidden>
+        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" hidden>Projects</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4" hidden data-bs-toggle="modal" data-bs-target="#modal-about" >About Us</button>
       </div>
     </div>
     <div class="overflow-hidden" style="max-height: 30vh;">
